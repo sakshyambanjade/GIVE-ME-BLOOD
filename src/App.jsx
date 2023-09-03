a558de1d24b41b1bce89eaa8638b815888017eef
SAKSHYAM BANJADE <sakshyambanjade@gmail.com>
2023-09-03 19:39:53 +0545

    api error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-
+import { useState , useEffect} from 'react'
+import {fetchDataFromApi} from "./utils/api"
 
 function App() {
 
+  useEffect(() => {
+    apiTesting();
+    },[])
+
+    const apiTesting = () => {
+        fetchDataFromApi('/3/discover/movie') 
+        .then((res)=> {
+          console.log(res); 
+        })
+    }
   return (
     <>
       <div className="App">
-qw
+
       </div>
     </>
   )
