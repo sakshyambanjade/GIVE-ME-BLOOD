e1621b25c3fa73fe4968f3358b957eb42b88024b
SAKSHYAM BANJADE <sakshyambanjade@gmail.com>
2023-09-06 13:24:17 +0545

    api problem not solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { useState , useEffect} from 'react'
-import {fetchDataFromApi} from "./utils/api"
+import { useState, useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api';
 
 function App() {
-
   useEffect(() => {
     apiTesting();
-    },[])
+  }, []);
 
-    const apiTesting = () => {
-        fetchDataFromApi('/3/discover/movie') 
-        .then((res)=> {
-          console.log(res); 
-        })
+  const apiTesting = async () => {
+    try {
+      const response = await fetchDataFromApi('/3/discover/movie');
+      console.log(response.data); // Assuming response.data contains the data you need
+    } catch (error) {
+      console.error('Error:', error);
     }
-  return (
-    <>
-      <div className="App">
+  };
 
-      </div>
-    </>
-  )
+  return (
+    <div className="App">
+      {/* Your component content here */}
+    </div>
+  );
 }
 
-export default App
+export default App;
