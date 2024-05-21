aa3f6d7f13281daf6298e08199089e6362c789c5
Your Name <sakshyambanjade@gmail.com>
2024-05-21 16:00:43 +0545

    made backend more functional register page

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const userModel = mongoose.model("users", userSchema);
+module.exports = userModel;
