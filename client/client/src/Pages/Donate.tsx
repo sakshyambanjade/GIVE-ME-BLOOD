d9307f309148e5f0ad3f3769ed4e52b21220f745
Your Name <sakshyambanjade@gmail.com>
2024-05-27 19:24:36 +0545

    added form for donation

--- client/client/src/Pages/Donate.tsx
+++ client/client/src/Pages/Donate.tsx
@@ -3,12 +3,14 @@ import Navbar from '../Components/Navbar'
 import Flex from '../Components/vflex'
 import Testimonial from '../Components/testimonial'
 import Quotes from '../Components/quotes'
+import Form from '../Components/form'
 function Donate() {
   return (
     <>
     <div className="navbar"> <Navbar/></div>
    <div className="Testimonial"><Testimonial/></div>
    <div className="quotes"><Quotes/></div>
+   <div className="form"><Form/></div>
    
     </>
   )
